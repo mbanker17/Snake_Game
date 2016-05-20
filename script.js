118141655c3cd931a1d71b1a4eecbe5854f89a61
mbanker17 <maggiebanker98@gmail.com>
2016-05-20 10:59:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,141 @@
 
-//We are starting with a function to run the entire game...
 (function() {
-    //JavaScript checks for canvas
-    var canvas = document.getElementById("canvas");
-    //Set 2d rendering content
-    var ctx = canvas.getContext("2d");
-    //set variable for game width and height to canvas              //width and height
-    var w = canvas.width;
-    var h = canvas.height;
-    //Lets save the cell width in a variable for easy control
-    var cw = 20;
-    //variable for direction....
-    var d;
-    //variable for food
-    var food;
-    //variable for score
-    var score;
-    
-    //Lets create the snake now
-    var snake_array; //an array of cells to make up the snake
-    //section 8 made it here.... slackerssssssssssssss
-    function init()
-    {
-        d = "right"; //default direction
-        create_snake();
-        create_food(); //Now we can see the food particle
-        //finally lets display the score
-        score = 0;
-        
-        //Lets move the snake now using a timer which will trigger the paint function
-        //every 60ms
-        if(typeof game_loop != "undefined") clearInterval(game_loop);
-        game_loop = setInterval(paint, 80);
-    }
-    init();
-    
-    function create_snake()
-    {
-        var length = 6; //Length of the snake
-        snake_array = []; //Empty array to start with
-        for(var i = length-1; i>=0; i--)
-        {
-            //This will create a horizontal snake starting from the top left
-            snake_array.push({x: i, y:0});
-        }
-    }
-    
-    //Lets create the food now
-    function create_food()
-    {
-        food = {
-            x: Math.round(Math.random()*(w-cw)/cw), 
-            y: Math.round(Math.random()*(h-cw)/cw), 
-        };
-        //This will create a cell with x/y between 0-40
-        //Because there are 40(400/10) positions accross the rows and columns
-        //Section 6 stopped here......
-    }
-    
-    //Lets paint  the canvas now
-    function paint()
-    {
-        //To avoid the snake trail we need to paint the BG on every frame
-        //Lets paint the canvas now
-        ctx.fillStyle = "pink";
-        ctx.fillRect(0, 0, w, h);
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(0, 0, w, h);
-        
-        //The movement code for the snake to be placed 
-        //The logic is simple
-        //Pop out the tail cell and place it infront of the head cell
-        var nx = snake_array[0].x;
-        var ny = snake_array[0].y;
- 
- 
+
+	var canvas = document.getElementById("canvas");
+	
+	var ctx = canvas.getContext("2d");
+
+	var w = canvas.width;
+	var h = canvas.height;
+	
+	var cw = 20;
+
+	var d;
+
+	var food;
+
+	var score;
+	
+
+	var snake_array; 
+	function init()
+	{
+		d = "right"; 
+		create_snake();
+		create_food(); 
+		score = 0;
+		
+		
+		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		game_loop = setInterval(paint, 100);
+	}
+	init();
+	
+	function create_snake()
+	{
+		var length = 8; 
+		snake_array = []; 
+		for(var i = length-1; i>=0; i--)
+		{
+			
+			snake_array.push({x: i, y:0});
+		}
+	}
+	
+	
+	function create_food()
+	{
+		food = {
+			x: Math.round(Math.random()*(w-cw)/cw), 
+			y: Math.round(Math.random()*(h-cw)/cw), 
+		};
+		
+	}
+	
+	
+	function paint()
+	{
+		
+		ctx.fillStyle = "#f7e6ff";
+		ctx.fillRect(0, 0, w, h);
+		ctx.strokeStyle = "purple";
+		ctx.strokeRect(0, 0, w, h);
+		
+	
+		var nx = snake_array[0].x;
+		var ny = snake_array[0].y;
+		
+		if(d == "right") nx++;
+		else if(d == "left") nx--;
+		else if(d == "up") ny--;
+		else if(d == "down") ny++;
+		
+	
+		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
+		{
+		
+			init();
+		
+			return;
+		}
+		
+	
+		if(nx == food.x && ny == food.y)
+		{
+			var tail = {x: nx, y: ny};
+			score++;
+		
+			create_food();
+		}
+		else
+		{
+			var tail = snake_array.pop();
+			tail.x = nx; tail.y = ny;
+		}
+	
+		snake_array.unshift(tail); 
+		for(var i = 0; i < snake_array.length; i++)
+		{
+			var c = snake_array[i];
+			
+			paint_cell(c.x, c.y);
+		}
+	
+		paint_cell(food.x, food.y);
+	
+		var score_text = "Score: " + score;
+		ctx.fillText(score_text, 100, h-100);
+	}
+	
+
+	function paint_cell(x, y)
+	{
+		ctx.fillStyle = "purple";
+		ctx.fillRect(x*cw, y*cw, cw, cw);
+		ctx.strokeStyle = "#550080";
+		ctx.strokeRect(x*cw, y*cw, cw, cw);
+	}
+	
+	function check_collision(x, y, array)
+	{
+	
+		for(var i = 0; i < array.length; i++)
+		{
+			if(array[i].x == x && array[i].y == y)
+			 return true;
+		}
+		return false;
+	}
+	
+	
+	document.onkeydown = function(e){
+		var key = e.which;
+		
+		if(key == "37" && d != "right") d = "left";
+		else if(key == "38" && d != "down") d = "up";
+		else if(key == "39" && d != "left") d = "right";
+		else if(key == "40" && d != "up") d = "down";
+		
+	};
+		
+})();
